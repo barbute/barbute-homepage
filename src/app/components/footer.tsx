f003a64b0c83eca095310759211cface7896e365
barbute <jackhdemeterio@gmail.com>
2024-11-28 15:51:25 -0600

    Add link styling

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -4,8 +4,7 @@ const Footer = () => {
       <div className="flex flex-col justify-center items-center">
         <p>© 2024 barbute. All Rights Reserved.</p>
         <p>
-          Website built based on 
-          <a href="https://www.craftz.dog/" target="_blank">Takuya Matsuyama's website</a>.
+          Website built based on <span className="link"><a href="https://www.craftz.dog/" target="_blank">Takuya Matsuyama's website</a></span>.
         </p>
       </div>
     </div>
