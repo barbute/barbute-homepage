bea017eda5bc2b9cd6c57660818a63b582c50499
barbute <jackhdemeterio@gmail.com>
2024-11-27 20:59:07 -0600

    Remove everything for now

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import Navbar from "./components/navbar";
 
 export const metadata: Metadata = {
   title: "barbute | Home",
@@ -15,7 +14,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en"> 
-      <Navbar />
       <body>
         {children}
       </body>
