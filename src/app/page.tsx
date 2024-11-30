4057fd2fba053562beb46878ca6492f796a95f50
barbute <jackhdemeterio@gmail.com>
2024-11-29 21:54:09 -0600

    Add info about me
    TODO Migrate this stuff to some type of component. Will be pain to edit otherwise
    
    TODO Add links/buttons for my socials

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       <div className="h-20"></div>
 
       {/* Main */}
-      <article className="flex flex-col justify-center items-center max-w-lg mx-auto">
+      <article className="flex flex-col justify-center max-w-lg mx-auto">
         <div className="mb-3">
           <Image
             src={barbutehero}
@@ -25,27 +25,86 @@ export default function Home() {
         <Callout 
           icon="👋"
           text={[
-            <p>Lorem ipsum odor amet, consectetuer adipiscing elit. Cubilia accumsan dictumst pellentesque vestibulum risus.</p>,
+            <p>Hello, I'm a high school senior passionate about computer science!</p>,
           ]}
         />
+        <h1 className="self-start mt-4">Jack</h1>
         <Section 
-          title={"About"} 
+          title={"Education"} 
           content={[
-            <p>Lorem ipsum odor amet, <Anchor link="https://www.google.com" text="consectetuer"/> adipiscing elit. Cubilia accumsan dictumst pellentesque vestibulum risus. Mus congue phasellus dolor lacus fusce mi sollicitudin. Conubia est lacinia nec integer suscipit tincidunt. Interdum tempus nisi class ut netus turpis ex. Phasellus purus sodales nulla iaculis rhoncus fusce. Convallis curabitur nisi porta velit iaculis himenaeos nisi pretium proin. Mus class fames molestie sem habitant.</p>,
-            <p>Quis porttitor montes scelerisque, molestie per condimentum! Molestie non orci pretium rhoncus iaculis habitasse sagittis augue. Curabitur arcu rutrum ridiculus sagittis quam ridiculus quis efficitur. Mi elementum dignissim etiam egestas aliquet mi! Magna erat tristique massa commodo auctor. Himenaeos suscipit nec torquent tortor, elit ultricies eros purus. Quisque hac sollicitudin blandit habitant vel egestas dictumst amet. Praesent pulvinar auctor; feugiat sapien cubilia dictum pulvinar. Habitant potenti velit suspendisse conubia cursus porta maecenas.</p>,
+            <div className="flex flex-col justify-center">
+              {/* Entry */}
+              <div className="flex items-center mb-3">
+                <p className="mr-3 font-bold bg-transparent px-2 py-1 rounded-lg">
+                  2021 - 2023
+                </p>
+                <div className="flex flex-col justify-center">
+                  <p className="underline underline-offset-2">Prosper High School</p>
+                  <ul className="">
+                    <li>- Computer Science I</li>
+                    <li>- AP Computer Science A</li>
+                  </ul>
+                </div>
+              </div>
+              {/* Entry */}
+              <div className="flex items-center mb-3">
+                <p className="mr-3 font-bold bg-transparent px-2 py-1 rounded-lg">
+                  2023 - Pres
+                </p>
+                <div className="flex flex-col justify-center">
+                  <p className="underline underline-offset-2">Walnut Grove High School</p>
+                  <ul className="">
+                    <li>- Computer Science III</li>
+                  </ul>
+                </div>
+              </div>
+            </div>,
           ]}
         />
         <Section 
           title={"Work"} 
           content={[
-            <p>Litora etiam blandit scelerisque dictum himenaeos leo bibendum dapibus facilisis. Duis ligula parturient eros turpis mus viverra suscipit est. Pretium donec hendrerit posuere sollicitudin magna urna scelerisque. Molestie felis id; cras ligula auctor natoque eget praesent? Convallis consectetur faucibus montes nec sem cras amet convallis hendrerit. Netus lobortis nascetur sit parturient habitasse. Montes natoque at sem vulputate in himenaeos iaculis.</p>,
-          ]}
-        />
-        <Section 
-          title={"More"} 
-          content={[
-            <p>Lorem ipsum odor amet, <Anchor link="https://www.google.com" text="consectetuer"/> adipiscing elit. Cubilia accumsan dictumst pellentesque vestibulum risus. Mus congue phasellus dolor lacus fusce mi sollicitudin. Conubia est lacinia nec integer suscipit tincidunt. Interdum tempus nisi class ut netus turpis ex. Phasellus purus sodales nulla iaculis rhoncus fusce. Convallis curabitur nisi porta velit iaculis himenaeos nisi pretium proin. Mus class fames molestie sem habitant.</p>,
-            <p>Quis porttitor montes scelerisque, molestie per condimentum! Molestie non orci pretium rhoncus iaculis habitasse sagittis augue. Curabitur arcu rutrum ridiculus sagittis quam ridiculus quis efficitur. Mi elementum dignissim etiam egestas aliquet mi! Magna erat tristique massa commodo auctor. Himenaeos suscipit nec torquent tortor, elit ultricies eros purus. Quisque hac sollicitudin blandit habitant vel egestas dictumst amet. Praesent pulvinar auctor; feugiat sapien cubilia dictum pulvinar. Habitant potenti velit suspendisse conubia cursus porta maecenas.</p>,
+            <p className="font-bold text-lg">High School Robotics</p>,
+            <p>On the <Anchor link="https://www.prosperengineeringteam.com" text="Prosper Engineering Team"/> I programmed control systems for robots designed to compete in the <Anchor link="https://www.firstinspires.org/robotics/frc" text="First Robotics Competition (FRC)"/>. The robot programs were written in Java.</p>,
+            <div className="flex flex-col justify-center">
+              {/* Entry */}
+              <div className="flex items-center mb-3">
+                <p className="mr-3 font-bold bg-transparent px-2 py-1 rounded-lg">
+                  2023
+                </p>
+                <div className="flex flex-col justify-center">
+                  <p className="underline underline-offset-2">
+                    FRC Team 9105
+                  </p>
+                  <ul className="">
+                    <li>- Rookie year</li>
+                    <li>- Programmed control system for a four DC Brushless motor tank drive</li>
+                    <li>- Designed consistent autonomous routines for scoring game peices without operator input</li>
+                  </ul>
+                </div>
+              </div>
+              {/* Entry */}
+              <div className="flex items-center mb-3">
+                <p className="mr-3 font-bold bg-transparent px-2 py-1 rounded-lg">
+                  2024
+                </p>
+                <div className="flex flex-col justify-center">
+                  <p className="underline underline-offset-2">
+                    FRC Team 9105
+                  </p>
+                  <ul className="">
+                    <li>- Migrated programming team to using an advanced data-logging framework</li>
+                    <li>- Created interfaces to interact with the robot's hardware</li>
+                  </ul>
+                  <p className="underline underline-offset-2">
+                    FRC Team 5411
+                  </p>
+                  <ul className="">
+                    <li>- Created software cirriculum for teaching new students about controlling a robot arm</li>
+                  </ul>
+                </div>
+              </div>
+            </div>,            
           ]}
         />
       </article>
