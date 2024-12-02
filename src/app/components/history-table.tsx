b9134902af81155af87c4cfcd340cce7567a47b6
barbute <jackhdemeterio@gmail.com>
2024-12-02 14:54:10 -0600

    Boilerplate for creating a history table

---
+++ src/app/components/history-table.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+interface HistoryTableProps {
+  title: any
+  rows: any[]
+}
+
+const HistoryTable: React.FC<HistoryTableProps> = ({ title, rows }) => {
+  return (
+    <></>
+  )
+};
+
+export default HistoryTable;
